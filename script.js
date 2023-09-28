2489dd44c0b8cdbe03375a4100fe4c0c2d193b89
Indrajeet Kumar <124766992+kumarindrajeetkumar717@users.noreply.github.com>
2023-09-28 19:58:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("mousemove",function(details) {
+
+   var rect = document.querySelector("#trangle");
+   var xval = gsap.utils.mapRange(
+    0,
+    window.innerWidth,
+    200 + rect.getBoundingClientRect().width/2,
+    window.innerWidth - (200 + rect.getBoundingClientRect().width/2),
+    details.clientX
+   );
+gsap.to("#trangle",{
+        left: xval,
+        ease: Power3,
+    });
+});
